1a168b514293e8b18b4bd7f6f469e5942d5013d8
tales <talesworks@gmail.com>
2021-11-07 21:04:48 +0800

    test read static file in functions

--- functions/listMeta/listMeta.js
+++ functions/listMeta/listMeta.js
@@ -4,21 +4,21 @@ const handler = (event) => {
   const idx = params.indexOf('in') || params.indexOf('listMeta');
   const [evName, slug] = params.splice(idx + 1);
 
-  let htmlText = fs.readFileSync(
-    require.resolve('./functions/listMeta/listMeta.html'),
-    'utf8',
-  );
-  // let htmlText = fs.readFileSync('./listMeta.html', 'utf8');
+  // let htmlText = fs.readFileSync(
+  //   require.resolve('./functions/listMeta/listMeta.html'),
+  //   'utf8',
+  // );
+  let htmlText = fs.readFileSync('./listMeta.html', 'utf8');
 
   htmlText = htmlText.replace('{{evName}}', evName + ' | ');
   htmlText = htmlText.replace('{{slug}}', slug);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      evName,
-      slug,
-    }),
-    // body: htmlText,
+    // body: JSON.stringify({
+    //   evName,
+    //   slug,
+    // }),
+    body: htmlText,
   };
 };
 
