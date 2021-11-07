a3796a17032b7082907c43974844844a8a0f51a5
tales <talesworks@gmail.com>
2021-11-07 19:55:45 +0800

    test for add static file in functions

--- functions/listMeta/listMeta.js
+++ functions/listMeta/listMeta.js
@@ -5,9 +5,11 @@ const handler = (event) => {
   const idx = params.indexOf('in') || params.indexOf('listMeta');
   const [evName, slug] = params.splice(idx + 1);
 
-  let htmlText = fs.readFileSync(require.resolve('./index.html'), 'utf8');
+  let htmlText = fs.readFileSync(require.resolve('./listMeta.html'), 'utf8');
+  // let htmlText = fs.readFileSync('./listMeta.html', 'utf8');
 
   htmlText = htmlText.replace('{{evName}}', evName + ' | ');
+  htmlText = htmlText.replace('{{slug}}', slug);
   return {
     statusCode: 200,
     // body: JSON.stringify({
