2acffe1b7a87b916a9c2947fa6d032880a640ee8
tales <talesworks@gmail.com>
2021-11-08 08:40:35 +0800

    test read static file in functions

--- functions/listMeta/listMeta.js
+++ functions/listMeta/listMeta.js
@@ -1,10 +1,15 @@
 const handler = (event) => {
+  // const path = require('path');
   const fs = require('fs');
+  // const resolved = (process.env.LAMBDA_TASK_ROOT)? path.resolve(process.env.LAMBDA_TASK_ROOT, fileName):path.resolve(__dirname, fileName)
+
+  // fs.readFile(resolved, "utf8", (err, results) => console.log(results))
+
   const params = event.path.split('/');
   const idx = params.indexOf('in') || params.indexOf('listMeta');
   const [evName, slug] = params.splice(idx + 1);
 
-  let htmlText = fs.readFileSync(require.resolve('./listMeta.html'), 'utf8');
+  let htmlText = fs.readFileSync('listMeta.html', 'utf8');
   // let htmlText = fs.readFileSync('./functions/listMeta/listMeta.html', 'utf8');
 
   htmlText = htmlText.replace('{{evName}}', evName + ' | ');
